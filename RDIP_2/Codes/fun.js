fa47806855ef01c06173eec238cf7611035d3825
Andole Sai Vamshi <vamshiandole646@gmail.com>
2020-09-18 19:49:44 +0530

    Goal10-unit6

--- RDIP_2/Codes/fun.js
+++ RDIP_2/Codes/fun.js
@@ -50,6 +50,7 @@ var prepositions = ['IN'];
 var conjunctions = ['CC'];
 var interjections = ['UH'];
 
+//--------------- My Functions ---------------//
 
 function clearfields() {
   subheading.innerHTML = '';
@@ -783,7 +784,7 @@ function procedureshow() {
   subheading.innerHTML = 'Procedure';
   document.getElementById('image1').style.display = 'none';
   description.innerHTML =
-    '<b><u>STEP1 :</u></b> Select a language from the drop down menu.<br><b><u>STEP2 :</u></b> Select a sentence from the drop down menu.<br><b><u>STEP3 :</u></b> Select corresponding POS for each word in the sentence and and click the <button>Submit</button> button<br><b><u>OUTPUT :</u></b> The submitted answer will be checked.<br><b><u>STEP4 :</u></b> If incorrect, click on <button>Get Answer</button> button for the correct answer or repeat STEP3.<br><br><br><hr>';
+    '<b><u>STEP1 :</u></b> Select a language from the drop down menu.<br><b><u>STEP2 :</u></b> Select a sentence from the drop down menu.<br><b><u>STEP3 :</u></b> Select corresponding POS for each word in the sentence and and click the <button>Submit</button> button<br><b><u>OUTPUT1 :</u></b> The submitted answer will be checked.<br><b><u>STEP4 :</u></b> If incorrect, click on <button>Get Answer</button> button for the correct answer or repeat STEP3.<br><br><br><hr>';
 }
 
 function furtherreadingsshow() {
